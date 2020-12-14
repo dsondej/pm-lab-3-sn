4579396d3e6e62ef7e1c2f4bd43daf5901fe5064
Patryk <patryk.lelakowski@student.wat.edu.pl>
2020-12-14 10:49:48 +0100

    Wykonano zadanie 3.3.1 – analogRead

--- Zadanie-3-2/src/main.cpp
+++ Zadanie-3-2/src/main.cpp
@@ -1,42 +1,23 @@
 #include <Arduino.h>
-#include <stdio.h>
 
-#define FOSC 16000000
-#define BAUD 9600
-#define MY_UBRR (FOSC / 16 / BAUD - 1)
-#define USE_UART_RX_IRQ 1
-char ReceivedChar;
-char Meesage[] = "Hello";
-char = pMessage = Message;
+#define ADC_RESOLUTION_DEFAULT (5.0f / 1024.0f)
+
+uint16_t digital;
+float voltage;
+
 void setup(){
-  UBRR0H = (MY_UBRR >> 8);
-  UBRR0L = MY_UBRR;
-  UCSR0B |= (1 << RXEN0) | (1 << TXEN0);
-  UCSR0C |= (1 << UCSZ01) | (1 << UCSZ00);
-
-  while(*pMessage){
-    while ( !( UCSR0A & (1<<UDRE0)) );
-    UDR0 = *pMessage;
-    pMessage++;
-  }
-
-#if (USE_UART_RX_IRQ == 1)
-  UCSR0B |= (1 << RXCIE0);
-#endif
+  Serial.begin(9600);
+  analogReference(DEFAULT);
+  pinMode(A0, input);
 }
+
 void loop(){
-#if (USE_UART_RX_IRQ == 0)
-  while (!(UCSR0A & (1 << RXC0)))
-  ;
-  ReceivedChar = UDR0;
-  while (!(UCSR0A & (1 << UDRE0)))
-  ;
-  UDR0 = ReceivedChar;
-#endif
+  digital = analogRead(A0);
+  voltage = ADC_RESOLUTION_DEFAULT * digital;
+  Serial.print("A0 = 0");
+  Serial.print(digital);
+  Serial.print(" \t V(0) = ");
+  Serial.print(voltage);
+  Serial.print(" [V]");
+  delay(1000);
 }
-#if (USE_UART_RX_IRQ == 1)
-ISR (USART_RX_vect){
-  ReceivedChar = UDR0;
-  UDR0 = ReceivedChar;
-}
-#endif
