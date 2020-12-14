4f6766d3bbb738dbebfe64d15e9d84c404a810d0
Maciej Osiak <maciej.osiak@student.wat.edu.pl>
2020-12-14 11:02:11 +0100

    Wykonano zadanie 3.3.2

--- Zadanie-3-3/src/main.cpp
+++ Zadanie-3-3/src/main.cpp
@@ -5,6 +5,23 @@
 uint16_t digital;
 float voltage;
 
+uint16_t ADCsingleREAD(uint8_t adcToUse)
+{
+  uint16_t ADCval;
+  
+  ADMUX = adcToUse;
+  ADMUX |= (1 << REFS0);
+  ADMUX &= ~(1 << ADLAR);
+  ADCSRA |= (1 << ADPS2) | (1 << ADPS1) | (1 <<ADPS0);
+  ADCSRA |= (1 << ADEN);
+  ADCSRA |= (1 << ADSC);
+  while (ADCSRA & (1 << ADSC))
+    ;
+  ADCval = ADCL;
+  ADCval = (ADCH << 8) + ADCval;
+
+  return ADCval;
+}
 void setup(){
   Serial.begin(9600);
   analogReference(DEFAULT);
