33816ceaaeeac77ad022c89e05b70b3c5a8c7185
Maciej Osiak <maciej.osiak@student.wat.edu.pl>
2020-12-14 20:53:50 +0100

    Wykonano zadanie 3.4.2

--- Zadanie-3-4/src/main.cpp
+++ Zadanie-3-4/src/main.cpp
@@ -1,21 +1,17 @@
 #include <Arduino.h>
 
+#include <avr/io.h>
+#include <avr/interrupt.h>
 
-#define PIN_G 5
-#define PIN_B 6
-#define PIN_R 9
+int main(void) {
+  DDRD &= ~(1 << DDD5);
+  PORTD |= (1 << PORTD5);
+  TIMSK1 |= (1 << TOIE1);
+  TCCR1B |= (1 << CS12) | (1 << CS11) | (1 << CS10);
+  sei();
 
-int r,g,b;
-
-void setup() {
-  pinMode(PIN_G, OUTPUT);
-  pinMode(PIN_B, OUTPUT);
-  pinMode(PIN_R, OUTPUT);
+  while (1) {}
 }
 
-void loop() {
-  analogWrite(PIN_R, rand());
-  analogWrite(PIN_G, rand());
-  analogWrite(PIN_B, rand());
-  delay(1000);
+ISR(TIMER1_OVF_vect) {
 }
