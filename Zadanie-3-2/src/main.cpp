a383c90108b6b552f3582528391938892ed7069a
Sebastian <sbeastian.kiszka@student.wat.edu.pl>
2021-02-24 12:54:57 +0100

    Wykonano zadanie 3.2.2 – zliczanie wciśnięć UART

--- Zadanie-3-2/src/main.cpp
+++ Zadanie-3-2/src/main.cpp
@@ -1,15 +1,39 @@
 #include <Arduino.h>
+#define BUTTON 2
 
-uint8_t i = 0;
+int stanPrzycisku;
+int poprzedniStanPrzycisku = LOW;
+unsigned long poprzedniCzas = 0;
+unsigned int liczba_nacisniec = 0;
 
-void setup()
+void setup() 
 {
   Serial.begin(9600);
-  Serial.println("Witaj programisto");
+  pinMode(BUTTON,INPUT);
 }
 
-void loop() 
+void loop()
 {
-  Serial.println(i);
-  delay(2000);
+  int odczyt = digitalRead(BUTTON);
+
+  if (odczyt != poprzedniStanPrzycisku)
+  {
+    poprzedniCzas = millis();
+  }
+
+  if ((millis() - poprzedniCzas) > 50) 
+  {
+    if (odczyt != stanPrzycisku) 
+    {
+      stanPrzycisku = odczyt;
+      if (stanPrzycisku == HIGH) 
+      {
+        liczba_nacisniec++;
+
+        Serial.print("Liczba nacisniec przycisku: ");
+        Serial.println(liczba_nacisniec);
+      }
+    }
+  }
+  poprzedniStanPrzycisku = odczyt;
 }
